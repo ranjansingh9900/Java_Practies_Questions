fa28ddb1ffae4903aabb6f9e00185b8b7309e5d8
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-04-19 20:10:01 +0530

    Update Basic_Questions.java

--- Basic_Questions.java
+++ Basic_Questions.java
@@ -258,6 +258,44 @@ class Main {
 
 
 
+// 9. Write a Java program to add two binary numbers.
+// Input Data:
+// Input first binary number: 10
+// Input second binary number: 11
+// Expected Output
+
+// Sum of two binary numbers: 101
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first b1: ");
+        long b1 = sc.nextLong();
+        System.out.println("enter the second b2: ");
+        long b2 = sc.nextLong();
+        int carry = 0;
+        int[] sum = new int[20];
+        int i = 0;
+        while (b1 != 0 || b2 != 0) {
+            sum[i++] = (int) ((b1 % 10 + b2 % 10 + carry) % 2);
+            carry = (int) ((b1 % 10 + b2 % 10 + carry) / 2);
+            b1 = b1 / 10;
+            b2 = b2 / 10;
+        }
+        if (carry != 0) {
+            sum[i++] = carry;
+        }
+        i--;
+        while (i >= 0) {
+            System.out.print(+sum[i--]);
+        }
+
+    }
+}
+
+
 
 
 
